97a0763a5a873bcf19abf318381d8ba2ef63a33c
Erick Teixeira <erickhenriquemota@gmail.com>
2020-10-05 22:12:58 -0300

    Gera projeto spring boot

---
+++ src/test/java/com/gerenciadorsessoesvotacao/GerenciadorSessoesVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciadorsessoesvotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorSessoesVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
